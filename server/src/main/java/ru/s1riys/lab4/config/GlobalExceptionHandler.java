19bc1426e9ff1aaa7f79fca0909a3a77b3793f47
S1riyS <kirill.ankudinov.94@mail.ru>
2025-01-09 21:25:25 +0500

    feat: proper global exceptions handler

--- server/src/main/java/ru/s1riys/lab4/config/GlobalExceptionHandler.java
+++ server/src/main/java/ru/s1riys/lab4/config/GlobalExceptionHandler.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
+import ru.s1riys.lab4.domain.dto.ErrorDetailsResponse;
+import ru.s1riys.lab4.domain.dto.ValidationErrorResponse;
+import ru.s1riys.lab4.exceptions.ApiException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,20 +18,42 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Object handleRequestValidationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
-        Map<String, Object> responseBody = new LinkedHashMap<>();
-
+    public ResponseEntity<ValidationErrorResponse> handleRequestValidationException(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request) {
         List<String> details = new ArrayList<String>();
         for (ObjectError error : ex.getBindingResult().getAllErrors()) {
             details.add(error.getDefaultMessage());
         }
 
-        responseBody.put("timestamp", new Date());
-        responseBody.put("status", HttpStatus.BAD_REQUEST.value());
-        responseBody.put("errors", details);
-        responseBody.put("path", request.getServletPath());
+        ValidationErrorResponse error = ValidationErrorResponse.builder()
+                .error(HttpStatus.BAD_REQUEST.name())
+                .title("Validation error")
+                .details(details)
+                .build();
 
-        return responseBody;
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(ApiException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorDetailsResponse> handleApiException(ApiException ex, HttpServletRequest request) {
+        ErrorDetailsResponse error = ErrorDetailsResponse.builder()
+                .error(ex.getStatus().name())
+                .title(ex.getTitle())
+                .details(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(error, ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<ErrorDetailsResponse> handleAnyException(Exception ex, HttpServletRequest request) {
+        ErrorDetailsResponse error = ErrorDetailsResponse.builder()
+                .error(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .title("Internal error")
+                .details("Internal error has occured")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
